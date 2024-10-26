6714ee6e1428f66b0967156f614b2ced34fec807
Estudiante <Estudiante@DESKTOP-C4C9BCJ>
2024-10-25 20:08:27 -0400

    subir

---
+++ Unidad3P1/ViewComponents/ProductosViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Unidad3P1.Data;
+using Unidad3P1.ViewModels;
+
+namespace Unidad3P1.ViewComponents
+{
+    public class ProductosViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public ProductosViewComponent(ApplicationDbContext dbContext,
+            IMapper mapper)
+        {
+            _context = dbContext;
+            _mapper = mapper;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int? id)
+        {
+            var entidades = await _context.Producto
+                .Where(x => x.CategoriaId == id)
+                .ToListAsync();
+            var modelsList = _mapper.Map<List<ProductoViewModel>>(entidades);
+            return View("MenuProductos", modelsList);
+        }
+    }
+}
