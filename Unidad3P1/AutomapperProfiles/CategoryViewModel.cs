df38d5ae205a6e39e8c7fdd26431f585ad435d95
Freddy Avalos Pardo <apf4003748@est.univalle.edu>
2024-11-08 14:46:12 -0400

    subir

---
+++ Unidad3P1/AutomapperProfiles/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Unidad3P1.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int CategoriaId { get; set; }
+        [Required]
+        public string CategoriaNombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ webapi/webApi/AutomapperProfiles/CategoryProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Unidad3P1.Data.Entidades;
+using Unidad3P1.ViewModels;
+
+
+namespace Unidad3P1.AutomapperProfiles
+{
+    public class CategoryProfile : Profile
+    {   
+        public CategoryProfile() 
+        {
+            CreateMap<CategoriaEntity, CategoryViewModel>()
+                .ForMember(dest => dest.CategoriaId, opt => opt.MapFrom(src => src.CategoriaId))
+                .ForMember(dest => dest.CategoriaNombre, opt => opt.MapFrom(src => src.CategoriaNombre))
+                .ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.Descripcion))
+                .ReverseMap();
+        }
+
+    }
+}
